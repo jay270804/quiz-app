14ed67cc8402655705d5527b4790b9970d00013b
jay270804 <jaypatel270804@gmail.com>
2024-12-23 14:15:51 +0530

    bug/added a flag to not seed questions if already exists

--- quiz/management/commands/seed_questions.py
+++ quiz/management/commands/seed_questions.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand
-from django.db import IntegrityError
 from quiz.models import Questions
 from quiz.config import SeedConfig
 from quiz.serializers import QuestionsSerializer
@@ -12,15 +11,17 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         """method to start seeding."""
         seed_data = SeedConfig.SEED_DATA
+        if not Questions.objects.exists():
+            for data in seed_data:
+                serializer = QuestionsSerializer(data=data)
 
-        for data in seed_data:
-            serializer = QuestionsSerializer(data=data)
-
-            if serializer.is_valid():
-                try:
-                    serializer.save()
-                    self.stdout.write(f"Created Question: {data['question_text']}")
-                except Exception as e:
-                    self.stderr.write(f"Failed to save question - {data['question_text']} - Error: {str(e)}")
-            else:
-                self.stderr.write(f"Validation error: {serializer.errors}")
+                if serializer.is_valid():
+                    try:
+                        serializer.save()
+                        self.stdout.write(f"Created Question: {data['question_text']}")
+                    except Exception as e:
+                        self.stderr.write(f"Failed to save question - {data['question_text']} - Error: {str(e)}")
+                else:
+                    self.stderr.write(f"Validation error: {serializer.errors}")
+        else:
+            self.stdout.write("Questions Already exists in DB, seeding not required.")
